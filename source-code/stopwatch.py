2a0a6008cc2152edf11338edbaed9d20ff8a81df
AbdullahFarhan419 <141566198+AbdullahFarhan419@users.noreply.github.com>
2023-10-26 20:28:13 +0700

    Stopwatch.py

--- source-code/stopwatch.py
+++ source-code/stopwatch.py
@@ -1,32 +1,52 @@
-#buat stopwatch
 import tkinter as tk
 
-#inisiasi tkinter
+# Inisiasi tkinter
 root = tk.Tk()
 
-# label
-stopwatch = tk.Label(root, text="Test")
+# Label
+stopwatch = tk.Label(root, text="00:00")
 stopwatch.pack()
 
 menit = 0
 detik = 0
-
+berjalan = True
 def update_stopwatch():
     global menit
     global detik
+    
+    if berjalan:
+        if detik < 59:
+            detik += 1
+        elif detik == 59:
+            detik = 0
+            menit += 1
 
-    if detik < 59:
-        detik += 1
-    elif detik == 59:
-        detik = 0
-        menit +=1
-
-    # perbarui Label.
+    # Perbarui Label.
     time_string = "{:02d}:{:02d}".format(menit, detik)
     stopwatch.config(text=time_string)
 
-    root.after(1000, update_stopwatch)  #memanggil kembali dalam 1000 ms
+    root.after(1000, update_stopwatch)  # Memanggil kembali dalam 1000 ms
 
+update_stopwatch()  # Mulai mengupdate stopwatch
 
-update_stopwatch() #mulai mengupdet stopwatch
+# Fungsi untuk menghentikan dan melanjutkan stopwatch
+def stop_stopwatch():
+    global berjalan
+    berjalan = not berjalan
+
+# Fungsi untuk mereset stopwatch
+def reset_stopwatch():
+    global menit, detik
+    menit = 0
+    detik = 0
+    stopwatch.config(text="00:00")
+
+# Tombol "Stop"
+stop_button = tk.Button(root, text="Stop", command=stop_stopwatch)
+stop_button.pack()
+
+# Tombol "Reset"
+reset_button = tk.Button(root, text="Reset", command=reset_stopwatch)
+reset_button.pack()
+
 root.mainloop()
